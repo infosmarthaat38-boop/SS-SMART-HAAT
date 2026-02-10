651950ad1654cd46af372a8a88ce0ed292a40ffe
infosmarthaat38-boop <info.smarthaat38@gmail.com>
2026-02-10 16:34:49 +0600

    Update full project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const animate = require("tailwindcss-animate")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+}
